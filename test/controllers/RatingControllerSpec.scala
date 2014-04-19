7fe7a52a76ea0e3b456118aa32023a15379ace26
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2014-04-19 12:45:12 -0300

    Updated RatingControllerSpec.scala

---
+++ test/controllers/RatingControllerSpec.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class RatingControllerSpec extends Specification {
+  "render a json in the index action" in {
+    val result = controllers.RatingController.index()(FakeRequest())
+    
+    status(result) must equalTo(OK)
+    contentAsString(result) must contain("ripple")
+  }
+}
