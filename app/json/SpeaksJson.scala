5d17f5d4b4854abf8806f200894923497499f499
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2014-04-20 14:53:38 -0300

    Updated SpeaksJson.scala

--- app/json/SpeaksJson.scala
+++ app/json/SpeaksJson.scala
@@ -5,17 +5,17 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 trait SpeaksJson {
-    implicit val ratingWrites: Writes[Rating] = (
-        (JsPath \ "developer").write[Developer] and
-        (JsPath \ "stars").write[Int]
-    )(Rating.toJson _)
-    
     implicit val developerWrites: Writes[Developer] = (
         (JsPath \ "id").writeNullable[Int] and
         (JsPath \ "name").write[String] and
         (JsPath \ "email").write[String]
     )(unlift(Developer.unapply))
-    
+
+    implicit val ratingWrites: Writes[Rating] = (
+        (JsPath \ "developer").write[Developer] and
+        (JsPath \ "stars").write[Int]
+    )(Rating.toJson _)
+
     implicit val projectWrites: Writes[Project] = (
         (JsPath \ "guid").write[String] and
         (JsPath \ "ratings").write[Seq[Rating]]
